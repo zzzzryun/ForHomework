f68e08c38c0d0165710c7d8595c0007df5a6033c
zzzzryun <202610780sangmyung.kr>
2026-03-13 11:55:25 +0900

    feat : 원 반지름으로 둘레, 넓이 구하는 기능 구현 - CircleCal.py 전부 수정

--- CircleCal.py
+++ CircleCal.py
@@ -1,3 +1,8 @@
+import math
+
 radius = float(input("원의 반지름을 입력해주세요 : "))
 
-circumferene = radius * 3.14 * 2
+circumferene = round(2 * math.pi * radius, 3)
+area = round(math.pi * (radius ** 2), 3)
+
+print(f"원의 둘레는 {circumferene}, 원의 넓이는 {area} 입니다")
