b80c2b4666524eaaccc5ae990b32669b41ebe8ec
zzzzryun <202610780sangmyung.kr>
2026-04-02 16:40:26 +0900

    feat : 한 줄씩 구구단 출력 구현

--- gugudan.py
+++ gugudan.py
@@ -1,6 +1,9 @@
-i, dan = 0, 0
+i,dan = 0, 0
 
 for dan in range(2,10):
-    print(f"#  {dan}단  #")
-    for i in range(1,10):
-        print(f"{dan}X{i:3.0f}={dan*i:3.0f}")
+    print(f"#  {dan}단  #", end="\t")
+print()
+for i in range(1,10):
+    for dan in range(2,10):
+        print(f"{dan}X{i:3.0f}={dan*i:3.0f}", end="\t")
+    print()
