30538b9b9a1df1a59d0f12c3f9bcdfc5b866eb98
zzzzryun <202610780sangmyung.kr>
2026-04-09 17:27:16 +0900

    feat : 정답 참고하여 함수 선언 - 3~5줄 추가, 11줄 수정

--- lotto.py
+++ lotto.py
@@ -1,11 +1,14 @@
 import random
 
+def getNumber():
+    return random.randrange(1,46)
+
 print("** 로또 추첨을 시작합니다. **\n")
 
 lotto = []
 
 while len(lotto) < 6:
-    ball = random.randrange(1,46)
+    ball = getNumber()
     if ball not in lotto:
         lotto.append(ball)
 
